0022ced26e9624ed83dba7c9c353e14409e7c46e
Andraz Vrhovec <andraz@koofr.net>
2020-12-05 18:26:52 +0100

    strip, not trim

--- sledilnik/settings/kube.py
+++ sledilnik/settings/kube.py
@@ -3,5 +3,5 @@ from .base import *
 
 ## read all settings from environment configmap
 
-ALLOWED_HOSTS = list(map(str.trim, os.getenv('ALLOWED_HOSTS', '*').split(',')))
+ALLOWED_HOSTS = list(map(str.strip, os.getenv('ALLOWED_HOSTS', '*').split(',')))
 SECRET_KEY = os.getenv('SECRET_KEY')
