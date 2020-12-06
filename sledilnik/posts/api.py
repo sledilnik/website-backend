43e9bc850a44471f78db972923f60fe8346a5c04
Joh Dokler <joh.dokler@gmail.com>
2020-12-06 16:38:56 +0100

    Fix image URLs and add object IDs

--- sledilnik/posts/api.py
+++ sledilnik/posts/api.py
@@ -10,12 +10,16 @@ class PostResource(ModelResource):
     class Meta:
         queryset = Post.objects.filter(published=True)
         resource_name = "posts"
-        fields = ["created", "updated", "author", "title", "image", "blurb", "link_to"]
+        fields = ["id", "created", "updated", "author", "title", "image", "blurb", "link_to", "body"]
 
     def dehydrate(self, bundle):
         lang = bundle.request.GET.get("lang")
+
         if lang not in [lng[0] for lng in settings.LANGUAGES]:
             lang = settings.LANGUAGE_CODE
         translation.activate(lang)
+
+        if bundle.obj.image:
+            bundle.data["image"] = bundle.request.build_absolute_uri(bundle.obj.image.url)
 
         return bundle
