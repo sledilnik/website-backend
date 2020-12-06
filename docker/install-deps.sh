8f0524fc532d4c505bea97fc702d8f391763a6d1
Andraz Vrhovec <andraz@koofr.net>
2020-12-06 18:26:49 +0000

    bump circus and uwsgi version

--- docker/install-deps.sh
+++ docker/install-deps.sh
@@ -5,4 +5,4 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 apt-get install -y nginx build-essential python3-dev libpq-dev libpq5
-pip3 --disable-pip-version-check --no-cache-dir install circus==0.16.1 uwsgi==2.0.18 pipenv
+pip3 --disable-pip-version-check --no-cache-dir install circus==0.17.1 uwsgi==2.0.19.1 pipenv==2020.11.15
