069f4c4af128e0ff925e5a975ba33db4234d1834
Joh Dokler <joh.dokler@gmail.com>
2020-12-09 23:25:06 +0100

    Develop settings

--- sledilnik/settings/develop.py
+++ sledilnik/settings/develop.py
@@ -1,2 +1,23 @@
 from .base import *
 from .secrets import *
+
+# DEBUG = False
+
+# ALLOWED_HOSTS = ["127.0.0.1"]
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'sledilnik',
+#     }
+# }
+
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+#         'LOCATION': 'default',
+#         'TIMEOUT': 60,
+#     }
+# }
+
+CACHE_MIDDLEWARE_SECONDS = 0
