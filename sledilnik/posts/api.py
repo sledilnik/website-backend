80497e3e6dfbb1b690fb084d2f8a74514bef7054
Joh Dokler <joh.dokler@gmail.com>
2020-12-10 03:59:41 +0100

    Model optimizations

--- sledilnik/posts/api.py
+++ sledilnik/posts/api.py
@@ -1,3 +1,5 @@
+import datetime
+
 from tastypie.resources import ModelResource
 
 from django.conf import settings
@@ -8,9 +10,12 @@ from .models import Post
 
 class PostResource(ModelResource):
     class Meta:
-        queryset = Post.objects.filter(published=True)
         resource_name = "posts"
+        queryset = Post.objects.all()
         fields = ["id", "created", "updated", "author", "title", "image", "blurb", "link_to", "body"]
+
+    def get_object_list(self, request):
+        return super().get_object_list(request).filter(published=True, created__lte=datetime.datetime.now())
 
     def dehydrate(self, bundle):
         lang = bundle.request.GET.get("lang")
--- sledilnik/posts/models.py
+++ sledilnik/posts/models.py
@@ -10,10 +10,10 @@ class Post(models.Model):
     created = models.DateTimeField(_("Created"), db_index=True)
     updated = models.DateTimeField(_("Updated"), auto_now=True)
 
-    author = models.CharField(_("Author"), max_length=100, null=True, blank=True)
-    image = models.ImageField(_("Image"), upload_to="posts/%Y/%m/%d/%H/%M/%S", null=True, blank=True)
     title = models.CharField(_("Title"), max_length=200)
     link_to = models.URLField(_("Link to"), null=True, blank=True)
+    image = models.ImageField(_("Image"), upload_to="posts/%Y/%m/%d/%H/%M/%S", null=True, blank=True)
+    author = models.CharField(_("Author"), max_length=100, null=True, blank=True)
     blurb = MarkdownField(_("Blurb"))
     body = MarkdownField(_("Body"), null=True, blank=True)
 
