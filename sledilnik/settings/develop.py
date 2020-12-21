88cf8455df1a767697347de1145c42774dd056ad
Joh Dokler <joh.dokler@gmail.com>
2020-12-21 11:11:52 +0100

    Fix invalid PredictionData date field type

--- sledilnik/settings/develop.py
+++ sledilnik/settings/develop.py
@@ -5,21 +5,9 @@ INSTALLED_APPS += [
     'django_extensions'
 ]
 
-# DEBUG = False
-
-# ALLOWED_HOSTS = ["127.0.0.1"]
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'sledilnik',
-#     }
-# }
-
-# CACHES = {
-#     'default': {
-#         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-#         'LOCATION': 'default',
-#         'TIMEOUT': 60,
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sledilnik',
+    }
+}
